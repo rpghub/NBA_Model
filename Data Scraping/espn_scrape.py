94aed531b6adf17fbf1c517add2384d8f2b52a99
Ryan Purcell <rscottpurcell@gmail.com>
2016-03-10 00:36:29 -0800

    update boxscore and game scrape for espn chng and add player scrape
    update boxscore function for changes in espn website and add player
    scrape function to get data from espn player pages

--- Data Scraping/espn_scrape.py
+++ Data Scraping/espn_scrape.py
@@ -6,6 +6,8 @@ import subprocess
 import time
 import numpy as np
 from datetime import datetime, date
+import traceback
+import re
 
 __author__ = 'Ryan'
 
@@ -23,8 +25,8 @@ def main_games():
     engine = create_engine(conn)
 
     # get list of game ids
-    teams = pd.read_sql('teampython',engine)
-    years = list(range(2015,2016))
+    teams = pd.read_sql('stgteam',engine)
+    years = list(range(2002, 2016))
     for year in years:
         print("start year " +str(year) + "\n"
               + time.strftime("%H:%M:%S",time.localtime()))
@@ -63,27 +65,60 @@ def main_pbp():
                               , if_exists = "append",chunksize=20000)
         print("finished - "+ time.strftime("%H:%M:%S",time.localtime()))
 
-
-
 def main_box_score():
     conn = 'mysql+mysqlconnector://ptest:nimda1@localhost/stg'
     engine = create_engine(conn)
     n = 200
-    sql = 'select * from stg.games_missing'
+    sql = 'select * from stg.games_missing_all'
     games = pd.read_sql(sql, engine)
-    for l in list(range(1,len(games["match_id"]),n)):
+    s = requests.session()
+    for l in list(range(0,len(games["match_id"]),n)):
         l_high = min(l+n,len(games["match_id"]))
         print("starting " + str(l) + " to " + str(l_high) + "\n"
                + time.strftime("%H:%M:%S",time.localtime()))
-        players = get_player_box_score(games)
+        players = get_player_box_score(games.iloc[l:l_high], s)
         try:
             players.to_sql('stgboxscore',engine,index = False
-                           , if_exists = "replace", chunksize = 20000)
+                           , if_exists = "append", chunksize = 20000)
         except Exception as e:
             restart_server()
             engine = create_engine(conn)
             players.to_sql('stgboxscore',engine,index = False
-                           , if_exists = "replace", chunksize = 20000)
+                           , if_exists = "append", chunksize = 20000)
+
+
+def main_player():
+    conn = 'mysql+mysqlconnector://ptest:nimda1@localhost/stg'
+    engine = create_engine(conn)
+    n = 1
+    sql = 'select * from stg.view_box_players'
+    players = pd.read_sql(sql, engine)
+    s = requests.session()
+    for idx, row in players.iterrows():
+        if idx % n == 0:
+            print(idx)
+            if idx > 0:
+                try:
+                    plr.to_sql('stgbox_player',engine,index = False
+                               , if_exists = "append", chunksize = 20000)
+                except Exception as e:
+                    restart_server()
+                    engine = create_engine(conn)
+                    players.to_sql('stgbox_player',engine,index = False
+                                   , if_exists = "append", chunksize = 20000)
+            plr = get_player(row[1], row[0])
+        else:
+            plr = plr.append(get_player(row[1], row[0])
+                             , ignore_index = True)
+    try:
+        plr.to_sql('stgbox_player',engine,index = False
+                   , if_exists = "append", chunksize = 20000)
+    except Exception as e:
+        restart_server()
+        engine = create_engine(conn)
+        players.to_sql('stgbox_player',engine,index = False
+                       , if_exists = "append", chunksize = 20000)
+
 
 def get_team_list():
     url = 'http://espn.go.com/nba/teams'
@@ -113,7 +148,7 @@ def get_team_list():
 
 def get_game_results(teams,year):
     BASE_URL = 'http://espn.go.com/nba/team/schedule/_/name/{0}/year/{1}' \
-               '/seasontype/2/{2}'
+               '/seasontype/3/{2}'
     match_id = []
     dates = []
     home_team = []
@@ -221,70 +256,175 @@ def get_play_by_play(match_id):
     playByPlay = pd.DataFrame(dic)
     return(playByPlay)
 
-def get_player_box_score(games):
+def get_player_box_score(games, ses):
     BASE_URL = 'http://espn.go.com/nba/boxscore?gameId={0}'
-    print(games['match_id'][1])
-    r = requests.get(BASE_URL.format(games['match_id'][0]))
+    r = ses.get(BASE_URL.format(games.iloc[0]['match_id']))
+    first_cookies = r.cookies
     soup = BeautifulSoup(r.text)
     table = soup.find('table',attrs = {'class':'mod-data'})
     heads = table.find_all('thead')
-    headers = heads[0].find_all('tr')[1].find_all('th')[1:]
+    headers = heads[0].find_all('tr')[0].find_all('th')[1:]
     headers = [th.text for th in headers]
-    columns = ['id', 'team', 'player'] + headers
+    columns = ['id', 'team', 'starter', 'player', 'position', 'url'] + headers
     players = pd.DataFrame(columns=columns)
     for index in games['match_id']:
         try:
-            r = requests.get(BASE_URL.format(index))
+            r = ses.get(BASE_URL.format(index), cookies = first_cookies)
         except Exception as e:
             print("connection refused")
             time.sleep(120)
             try:
-                r = requests.get(BASE_URL.format(index))
+                r = ses.get(BASE_URL.format(index), cookies = first_cookies)
             except Exception:
                 return(players)
             pass
         soup = BeautifulSoup(r.text)
         try:
-            table = soup.find('table',attrs = {'class':'mod-data'})
-            heads = table.find_all('thead')
-            bodies = table.find_all('tbody')
+            table = soup.find_all('table',attrs = {'class':'mod-data'})
+            teams = soup.find_all('span', attrs = {'class':'team-name'})
+            bodies = table[0].find_all('tbody')
 
-            team_1 = heads[0].th.text
-            team_1_players = bodies[0].find_all('tr') + bodies[1].find_all('tr')
+
+            team_1 = teams[0].text
+            team_1_players = bodies[0].find_all('tr')
             team_1_players = get_player_box_score_helper(team_1_players
+                                                         , 'STARTER'
+                                                         , team_1, headers
+                                                         , columns, index)
+            players = players.append(team_1_players)
+            team_1_players = bodies[1].find_all('tr')
+            team_1_players = team_1_players[:len(team_1_players) - 2]
+            team_1_players = get_player_box_score_helper(team_1_players
+                                                         , 'BENCH'
                                                          , team_1, headers
                                                          , columns, index)
             players = players.append(team_1_players)
 
-            team_2 = heads[3].th.text
-            team_2_players = bodies[3].find_all('tr') + bodies[4].find_all('tr')
-            team_2_players = get_player_box_score_helper(team_2_players, team_2
+            bodies = table[1].find_all('tbody')
+            team_2 = teams[1].text
+            team_2_players = bodies[0].find_all('tr')
+            team_2_players = get_player_box_score_helper(team_2_players
+                                                         , 'STARTER'
+                                                         , team_2
+                                                         , headers, columns
+                                                         , index)
+            players = players.append(team_2_players)
+            team_2_players = bodies[1].find_all('tr')
+            team_2_players = team_2_players[:len(team_2_players) - 2]
+            team_2_players = get_player_box_score_helper(team_2_players
+                                                         , 'BENCH'
+                                                         , team_2
                                                          , headers, columns
                                                          , index)
             players = players.append(team_2_players)
         except Exception as e:
-            print("page load error")
+            print(traceback.format_exc())
             print(index)
+            urls = ['http://espn.go.com/mlb/team/_/name/bal/baltimore-orioles'
+                    , 'http://espn.go.com/'
+                    , 'http://espn.go.com/nba/team/_/name/gs/golden-state-warriors'
+                    , 'http://www.espnfc.us/scores'
+                    , 'http://espn.go.com/nfl/schedule'
+                    , 'http://espn.go.com/nba/schedule'
+                    , 'http://espn.go.com/nba/statistics'
+                    , 'http://espn.go.com/espn/news/story?id=5630562']
+            time.sleep(np.random.randint(1,60))
+            ses = requests.session()
+            r = ses.get(urls[np.random.randint(0,len(urls))])
+            r = ses.get(urls[np.random.randint(0,len(urls))])
+            first_cookies = r.cookies
             pass
 
     return(players)
 
-def get_player_box_score_helper(players, team_name,headers,columns,index):
-    array = np.zeros((len(players), len(headers)+1), dtype=object)
+def get_player_box_score_helper(players, starter, team_name, headers, columns, index):
+    array = np.zeros((len(players), len(headers)+3), dtype=object)
     array[:] = np.nan
     for i, player in enumerate(players):
-        cols = player.find_all('td')
-        array[i, 0] = cols[0].text.split(',')[0]
-        for j in range(1, len(headers) + 1):
-            if not cols[1].text.startswith('DNP'):
-                array[i, j] = cols[j].text
-
+        try:
+            cols = player.find_all('td')
+            array[i, 0] = cols[0].text[:len(cols[0].text) - 2]
+            array[i, 1] = cols[0].text[len(cols[0].text) - 2:]
+            array[i, 2] = cols[0].a['href']
+            for j in range(3, len(headers) + 3):
+                if not (cols[1].text.startswith('Did not play') or
+                cols[1].text.startswith('DNP')):
+                    array[i, j] = cols[j - 2].text
+        except Exception as e:
+            print('match_id: ' + str(index) + '\n' + 'player: '
+              + str(cols[0].text) + '\n' + team_name + ' ' + starter
+              +'\n' 'index: ' + str(i))
+            print(cols[0].text)
+            print(cols[j - 2].text)
     frame = pd.DataFrame(columns=columns)
     for x in array:
-        line = np.concatenate(([index, team_name], x)).reshape(1,len(columns))
+        line = np.concatenate(([index, team_name, starter], x)).reshape(1,len(columns))
         new = pd.DataFrame(line, columns=frame.columns)
         frame = frame.append(new)
     return frame
+
+def get_player(player_url, box_name):
+    r = requests.get(player_url)
+    soup = BeautifulSoup(r.text)
+    try:
+        table = soup.find_all('ul',attrs = {'class':'general-info'})
+        cols = table[0].find_all('li')
+        hw = re.sub("[^0-9,:']", "",cols[1].text).split(",")
+        height = hw[0].split("'")
+        height = int(height[0])*12 + int(height[1])
+        weight = int(hw[1])
+        player_name = soup.find('h1').text
+        table = soup.find_all('ul',attrs = {'class':'player-metadata floatleft'})
+        for idx, col in enumerate(table):
+            if idx == 0:
+                cols = col.find_all('li')
+            else:
+                cols = cols + col.find_all('li')
+        bdate = cols[0].text.replace('Born','')
+        if bdate.find('in') > 0:
+            bdate = bdate[:bdate.find('in')-1]
+        else:
+            bdate = bdate[:bdate.find('(Age:')-1]
+        bdate = datetime.strptime(bdate[:12], '%b %d, %Y').strftime('%Y-%m-%d')
+        if 'Died' in cols[1].text:
+            cols = [cols[0]] + cols[2:]
+        if 'Drafted' in cols[1].text:
+            draft = re.sub("[^0-9,:]", "",cols[1].text)
+            draft_year = draft[:draft.find(':')]
+            draft_rnd = draft[draft.find(':')+1:draft.find(',')]
+            draft_num = draft[draft.find(',')+1:]
+            draft_team = cols[1].text[cols[1].text.find('by')+3:]
+            college = cols[2].text.replace('College', '')
+            try:
+                experience = cols[3].text.replace('Experience', '')
+                experience = int(experience.replace('year', '').replace('s',''))
+            except IndexError:
+                experience = -1
+        else:
+            draft_year = -1
+            draft_rnd = -1
+            draft_num = -1
+            draft_team = ''
+            college = cols[1].text.replace('College', '')
+            try:
+                experience = cols[2].text.replace('Experience', '')
+                experience = int(experience.replace('year', '').replace('s',''))
+            except IndexError:
+                experience = -1
+    except Exception as e:
+        print(traceback.format_exc())
+        print('URL: ' + player_url)
+        #print(cols)
+        return
+    dic = {'player': player_name, 'player_lname': box_name, 'bdate': bdate
+           , 'draft_year': draft_year, 'draft_rnd': draft_rnd
+           , 'draft_num': draft_num, 'draft_team': draft_team
+           , 'college': college, 'exp': experience
+           , 'height': height, 'weight': weight
+           , 'url': player_url}
+    plr = pd.DataFrame(dic, index = [0])
+    return(plr)
+
 
 def restart_server():
     try:
@@ -292,7 +432,4 @@ def restart_server():
     except Exception as e:
         pass
     subprocess.call("NET START MySQL")
-
-
-
 
