0ef7f9ac2896eadfcf101704aa6d78b4a49db6f3
Ryan Purcell <rscottpurcell@gmail.com>
2016-03-10 00:39:22 -0800

    update ref_player and boxscore definitions
    update ref_player and boxscore tables to handel new data and fields

--- DB/create stg tables.sql
+++ DB/create stg tables.sql
@@ -2,28 +2,36 @@
 
 CREATE SCHEMA stg;
 
-CREATE TABLE stg.boxscore (
+CREATE TABLE boxscore (
   box_id integer auto_increment primary key,
   match_id integer,
   team varchar(30),
+  team_id int,
   player varchar(250),
+  player_id int,
+  position varchar(5),
+  starter varchar(10),
   min_ double,
-  fgm_a varchar(10),
-  pm3_a varchar(10),
-  ftm_a varchar(10),
+  fgm int,
+  fga int,
+  pm3 int,
+  pm3_a int,
+  ftm int,
+  fta int,
   oreb int,
   dreb int,
   reb int,
   ast int,
   stl int,
   blk int,
-  to_ int,
+  tov int,
   pf int,
-  pts int
+  pts int,
+  pm int
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE INDEX match_id_idx ON stg.boxscore (match_id);
-ALTER TABLE boxscore ADD UNIQUE INDEX match_id_unique (match_id, team, player); 
+ALTER TABLE boxscore ADD UNIQUE INDEX match_id_unique (match_id, team_id, player_id); 
 
 CREATE TABLE stg.games (
   match_id integer primary key,
@@ -79,6 +87,7 @@ CREATE TABLE stg.ref_teams (
   season_year integer,
   team_name varchar(60),
   city_name varchar(40),
+  team_abrv varchar(4),
   isDefault integer
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -119,8 +128,6 @@ CREATE INDEX REF_ACTION_ACTION_GRP_IDX ON stg.ref_action(action_grp_id);
 create index ref_action_type1_idx on ref_action(type1);
 create index ref_action_type2_idx on ref_action(type2);
 
-
-
 CREATE TABLE stg.cross_team (
   cross_team_id integer auto_increment primary key
   ,team_odd_id int
@@ -150,15 +157,53 @@ CREATE TABLE stg.game_odds (
 CREATE INDEX GAME_ODDS_TEAM_IDX ON stg.game_odds(team_id);
 CREATE INDEX GAME_ODDS_GAME_IDX ON stg.game_odds(match_id);
 
+DROP TABLE IF EXISTS PLAYER;
+CREATE TABLE player(
+  player_id int primary key,
+  bdate date,
+  college varchar(60),
+  draft_num int,
+  draft_rnd int,
+  draft_team varchar(5),
+  draft_year int,
+  exp int,
+  height int,
+  player varchar(60),
+  player_lname varchar(60),
+  url varchar(100),
+  weight int
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create index player_name_idx on player(player);
+create index player_lname_idx on player(player_lname);
+create index player_url_idx on player(url);
+
+
+create table ref_player_backup like ref_player;
+insert into ref_player_backup select * from ref_player;
+
+ DROP TABLE IF EXISTS stg.ref_player;
+ CREATE TABLE stg.ref_player(
+	ref_player_id INT AUTO_INCREMENT PRIMARY KEY
+    ,player_id INT
+    ,team_id INT
+    ,season_year INT
+    ,player VARCHAR(50)
+    ,isDefault INT
+    ,AllDefault INT
+)ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+create index all_idx on ref_player(player_id, team_id, season_year);
+create index player_idx on ref_player(player_id, AllDefault);
+create index all_defaualt_idx on ref_player(player_id, team_id, season_year, isDefault);
+create index player_all_idx on ref_player(player, team_id, season_year);
 
 /*CREATE VIEWS OF DATA TO QUERY*/
-
 CREATE VIEW stg.play_by_play_missing AS
 	SELECT g.* 
     FROM stg.games g 
 		LEFT JOIN stg.play_by_play p ON p.match_id = g.match_id
 	WHERE p.match_id IS NULL;
-    
     
 CREATE VIEW stg.games_missing AS
 	SELECT g.* 
@@ -166,5 +211,17 @@ CREATE VIEW stg.games_missing AS
 		LEFT JOIN stg.boxscore sg ON sg.match_id = g.match_id
 	WHERE sg.match_id IS NULL;
 
+drop view if exists games_missing_all;
+CREATE VIEW stg.games_missing_all AS
+	SELECT g.match_id
+    FROM games g 
+		LEFT JOIN stgboxscore sg ON sg.id = g.match_id
+	GROUP BY g.match_id
+	HAVING count(sg.id) < 16;
 
-select * from games_missing
+drop view if exists view_box_players;
+CREATE VIEW view_box_players AS
+	SELECT distinct s.player, s.url
+    FROM stgboxscore s
+      left join stgbox_player p on s.url = p.url
+     WHERE s.url is not null and p.player is null;
